91079f5624a13a5c656f829a096228df64ba6432
José Melo <jos.ryan.melo@gmail.com>
2026-03-08 09:49:01 -0300

    feat: busca total de dias que um funcionário possui de licença

--- src/jornadaLicenca.pl
+++ src/jornadaLicenca.pl
@@ -17,17 +17,19 @@ registrar_licenca(Cpf, Tipo, Duracao) :-
     verifica_licenca(Tipo, Duracao),
     assertz(licenca_registrada(Cpf, Tipo, Duracao)).
 
+% licenças já registradas
 licenca_registrada("11111111111", paternidade, 4).
 licenca_registrada("11111111111", casamento, 2).
 
 
-
+% auxiliar para verificar validade
 verifica_licenca(Tipo, Duracao) :-
     licenca(Tipo, Duracao),
     Duracao > 0,
     licenca(Tipo, Max),
     Duracao =< Max.
 
+% lista todas as licenças de um funcionário
 licencas_do_funcionario(Cpf, Lista) :-
     findall(
         licenca_registrada(Cpf,Tipo,Dias),
@@ -35,8 +37,20 @@ licencas_do_funcionario(Cpf, Lista) :-
         Lista
     ).
 
+% busca o total de dias de licença que um funcionário possui
+total_dias_licenca(Cpf, Total) :-
+    findall(
+        Dias,
+        licenca_registrada(Cpf,_,Dias),
+        ListaDias
+    ),
+    soma_dias_licenca(ListaDias, Total).
 
-
+% método auxiliar que soma recursivamente a quantidade de dias de licença de um funcionário
+soma_dias_licenca([], 0).
+soma_dias_licenca([H|T], R) :-
+    soma_dias_licenca(T, R1),
+    R is H + R1.
 
 
 
@@ -52,7 +66,7 @@ licencas_do_funcionario(Cpf, Lista) :-
 % registrar_licenca("11111111111", casamento, 3),
 % licencas_do_funcionario("11111111111", L).
 %
-%
+% total_dias_licenca("11111111111", X).
 %
 %
 %
